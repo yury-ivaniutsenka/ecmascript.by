d1b41dcd7d87883402dc926905052e8e1da10ff3
Yury <yury.ivaniutsenka@ximxim.com>
2016-10-11 22:12:01 +0300

    Express index.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hello World");
-    response.end();
-}).listen(80);
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(80);
