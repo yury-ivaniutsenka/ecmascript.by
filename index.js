8f9506c1418eecbc663b3f317882a3b8e27b6582
Yury <yury.ivaniutsenka@ximxim.com>
2016-10-11 22:18:41 +0300

    Add router.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+var routes = require('./routes/index');
+
+app.use('/', routes);
 
 app.listen(80);
